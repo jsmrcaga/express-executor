67fd094767f135f28537983e50b34984e188d4c9
Jo Colina <jo.colina@weezevent.com>
2021-08-30 12:00:46 +0200

    Tokens: auto generate only if env is present

--- test/build.js
+++ test/build.js
@@ -1,2 +1,12 @@
+const { expect } = require('chai');
 // No need for more, just test that everything is imported correctly
-require('../index');
+const _exports = require('../index');
+
+describe('Exports', () => {
+	it(`Should export every key`, () => {
+		for(const [k, v] of Object.entries(_exports)) {
+			expect(_exports[k]).not.to.be.undefined;
+			expect(v).not.to.be.undefined;
+		}
+	});
+});
