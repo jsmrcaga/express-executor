5976f43efa9c8df7640a997d8888792006facb2b
Jo Colina <jo.colina@weezevent.com>
2021-09-06 16:35:56 +0200

    Build: fix exports

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const Controller = require('./lib/controller');
 const View = require('./lib/view');
+const BaseView = require('./lib/views/view');
 const { Authorization } = require('./lib/middleware/authorization');
 const { Token, tokens, TokenGenerator } = require('./lib/token');
-const { GenericSerializer, DeserializationError, SerializationError } = require('./lib/serializers/serializers');
+const { GenericSerializer, BaseSerializer } = require('./lib/serializers/serializers');
 const Errors = require('./lib/errors');
 
 const Utils = require('./lib/utils');
 
 module.exports = {
 	Controller,
+	BaseView,
 	View,
 	Token,
 	tokens,
 	TokenGenerator,
 	Authorization,
 	Utils,
+	BaseSerializer,
 	GenericSerializer,
-	DeserializationError,
-	SerializationError,
 	Errors
 };
